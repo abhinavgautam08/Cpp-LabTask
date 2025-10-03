a356e8052f4c76defb61a0a763e961b0001211b8
Abhinav Adarsh <abhinavgautam08@users.noreply.github.com>
2025-10-03 20:01:56 +0530

    Write a C++ program to find all unique palindromic substrings in a given string. The function should take a string as input and return a set of strings containing all unique palindromic substrings.

---
+++ Prog8.cpp
@@ -0,0 +1,31 @@
+//Write a C++ program to find all unique palindromic substrings in a given 
+//string. The function should take a string as input and return a set of strings 
+//containing all unique palindromic substrings. 
+
+#include <iostream>
+#include <unordered_set>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string input;
+    cout << "Enter a string: ";
+    cin >> input;
+    unordered_set<string> palindromes;
+    int n = input.length();
+    for (int i = 0; i < n; ++i){
+        for (int j = i; j < n; ++j){
+            string sub = input.substr(i, j - i + 1);
+            string rev = string(sub.rbegin(), sub.rend());
+            if (sub == rev){
+                palindromes.insert(sub);
+            }
+        }
+    }
+    cout << "Unique palindromic substrings:\n";
+    for (const string &palindrome : palindromes){
+        cout << palindrome << "\n";}
+    
+    return 0;
+}
